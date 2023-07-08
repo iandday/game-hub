49d960aa6af5417a5384e35e53dbf6374465f145
Ian Day <ian@ianday.me>
2023-07-08 18:06:42 -0400

    filter games by platform

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
-import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -18,7 +15,14 @@ export interface Game {
     metacritic: number;
   }
 
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (selectedGenre: Genre | null, selectedPlatform: Platform | null) => 
+  useData<Game>(
+    '/games', 
+    {
+      params: {genres: selectedGenre?.id, platforms: selectedPlatform?.id}
+    }, 
+    [selectedGenre?.id, selectedPlatform?.id]
+  )
 
 
 
